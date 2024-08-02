6e1575506ff57ae998a093511d50c15a65a5bafb
millerding <mding022@uottawa.ca>
2024-08-02 11:21:18 -0400

    gif and video

--- src/main/java/com/aeapi/springboot/controller/AEAPIController.java
+++ src/main/java/com/aeapi/springboot/controller/AEAPIController.java
@@ -12,10 +12,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,8 +22,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.aeapi.springboot.service.AEService;
 import com.aeapi.springboot.service.impl.AEServiceImpl;
-
-import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
 public class AEAPIController {
@@ -50,9 +45,55 @@ public class AEAPIController {
 			System.out.println("Template not found!");
 			return null;
 		}
-
+		List<String> files = new ArrayList<String>();
 		try {
-			List<String> files = new ArrayList<String>();
+			
+
+			for(MultipartFile image : images) {
+				aeService.saveFile("ae/images/"+image.getOriginalFilename(), image);
+				files.add(image.getOriginalFilename());
+			}
+			
+
+			aeService.create(files, template, image_count);
+			Path def = Paths.get("ae/output/Render_Comp 1_00002.mp4");
+			String randomName = String.valueOf(ThreadLocalRandom.current().nextInt(1,1000000000));
+			Path ran = Paths.get("ae/output/"+randomName+".mp4");
+			Files.move(def, ran);
+
+			for(String file : files) {
+				Files.delete(Paths.get("ae/images/"+file));
+			}
+			System.out.println("File: " + randomName+".mp4");
+			return aeService.getVideo(randomName+".mp4");
+
+        } catch (IOException e) {
+			System.out.println("Error taking files");
+			for(String file : files) {
+				try {
+					Files.delete(Paths.get("ae/images/"+file));
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+        }
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+	@PostMapping("/create_gif")
+	public ResponseEntity<Resource> createGif(@RequestParam("images") List<MultipartFile> images, @RequestParam("template") String template) {
+        
+		aeService = new AEServiceImpl();
+		int image_count = 0;
+		try{image_count = aeService.getTemplates().get(template);System.out.println("count images: " + image_count);}
+		catch(Exception e) {
+			System.out.println("Template not found!");
+			return null;
+		}
+		List<String> files = new ArrayList<String>();
+		try {
+			
 
 			for(MultipartFile image : images) {
 				aeService.saveFile("ae/images/"+image.getOriginalFilename(), image);
@@ -72,12 +113,20 @@ public class AEAPIController {
 			aeService.ffmpeg("ae/output/"+randomName+".mp4", "ae/output/"+randomName+".gif");
 			System.out.println("File: " + randomName+".gif");
 			return aeService.getGif(randomName+".gif");
-
         } catch (IOException e) {
 			System.out.println("Error taking files");
+			for(String file : files) {
+				try {
+					Files.delete(Paths.get("ae/images/"+file));
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
         }
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
     }
+
 
 	@GetMapping("templates")
 	public Map<String, Integer> getTemplates() {
