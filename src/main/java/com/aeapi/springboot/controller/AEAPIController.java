002e8e5b3dd0147db3f9ff2a921c2cf56a340b13
millerding <mding022@uottawa.ca>
2024-08-15 13:48:22 -0400

    update cors mapping

--- src/main/java/com/aeapi/springboot/controller/AEAPIController.java
+++ src/main/java/com/aeapi/springboot/controller/AEAPIController.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins = {"http://localhost:3000", "https://millerding.com"})
 public class AEAPIController {
 
 	@Autowired
